ed4a9ca211235c43b5dc31ae35235f03b70d2085
jillianmeehan <jillianleighmeehan@gmail.com>
2024-11-03 18:21:52 -0500

    Added functionality for the select

--- javascript/index.js
+++ javascript/index.js
@@ -1,27 +1,48 @@
-function updateLosAngelesTime() {
+function updateTime() {
   let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
-  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+
+    let newYorkElement = document.querySelector("#new-york");
+    if (newYorkElement) {
+      let newYorkDateElement = newYorkElement.querySelector(".date");
+      let newYorkTimeElement = newYorkElement.querySelector(".time");
+      let newYorkTime = moment().tz("America/New_York");
+
+      newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+      newYorkTimeElement.innerHTML = newYorkTime.format(
+        "h:mm:ss [<small>]A[</small>]"
+      );
+    }
+  }
 }
 
-setInterval(updateLosAngelesTime, 1000);
-
-function updateNewYorkTime() {
-  let newYorkElement = document.querySelector("#new-york");
-  let newYorkDateElement = newYorkElement.querySelector(".date");
-  let newYorkTimeElement = newYorkElement.querySelector(".time");
-  let newYorkTime = moment().tz("America/New_York");
-
-  newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
-  newYorkTimeElement.innerHTML = newYorkTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace(`_`, ` `).split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time" id="new-york">${cityTime.format(
+            "h:mm:ss"
+          )} <small>${cityTime.format("A")}</small></div>
+        </div>`;
 }
 
-setInterval(updateNewYorkTime, 1000);
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+
+citiesSelectElement.addEventListener("change", updateCity);
