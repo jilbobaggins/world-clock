9e4c933d4668d014136fc7e219222955dd721d12
jillianmeehan <jillianleighmeehan@gmail.com>
2024-11-03 22:28:13 -0500

    Added two cities and altered styling

--- javascript/index.js
+++ javascript/index.js
@@ -20,6 +20,30 @@ function updateTime() {
       newYorkTimeElement.innerHTML = newYorkTime.format(
         "h:mm:ss [<small>]A[</small>]"
       );
+
+      let honoluluElement = document.querySelector("#honolulu");
+      if (honoluluElement) {
+        let honoluluDateElement = honoluluElement.querySelector(".date");
+        let honoluluTimeElement = honoluluElement.querySelector(".time");
+        let honoluluTime = moment().tz("Pacific/Honolulu");
+
+        honoluluDateElement.innerHTML = honoluluTime.format("MMMM Do YYYY");
+        honoluluTimeElement.innerHTML = honoluluTime.format(
+          "h:mm:ss [<small>]A[</small>]"
+        );
+
+        let chicagoElement = document.querySelector("#chicago");
+        if (chicagoElement) {
+          let chicagoDateElement = chicagoElement.querySelector(".date");
+          let chicagoTimeElement = chicagoElement.querySelector(".time");
+          let chicagoTime = moment().tz("America/Chicago");
+
+          chicagoDateElement.innerHTML = chicagoTime.format("MMMM Do YYYY");
+          chicagoTimeElement.innerHTML = chicagoTime.format(
+            "h:mm:ss [<small>]A[</small>]"
+          );
+        }
+      }
     }
   }
 }
